c87d0120a854a12a742525fb72a71f75758c7af1
배지영 <melissaeliza0@gmail.com>
2017-11-23 14:11:22 +0900

    no message

---
+++ WeatherForecast/View/ImageCoverView.swift
@@ -0,0 +1,21 @@
+//
+//  ImageCoverView.swift
+//  WeatherForecast
+//
+//  Created by 배지영 on 2017. 11. 23..
+//  Copyright © 2017년 baecheese. All rights reserved.
+//
+
+import UIKit
+
+class ImageCoverView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ WeatherForecast/View/WeatherStatusBackground.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherStatusBackground.swift
+//  WeatherForecast
+//
+//  Created by 배지영 on 2017. 11. 23..
+//  Copyright © 2017년 baecheese. All rights reserved.
+//
+
+import UIKit
+
+class WeatherStatusBackground: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
